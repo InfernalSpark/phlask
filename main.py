b6b52b5a79a64ab5cb4452b7fface18a3d0f8ea2
InfernalSpark <unnikiran976@gmail.com>
2021-07-26 21:08:57 +0530

    stuf

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, make_response
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/')
+def main():
+    print("")
+    return "WHAT NOW?"
+
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
     return render_template("login.html")
+    form = request.form
+    user = form['username']
+    print("GOT IT")
+    res = make_response()
+    res.set_cookie('username', user)
+
+if __name__ == '__main__':
+    app.run(debug = True)
